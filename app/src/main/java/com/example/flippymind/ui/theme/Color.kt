e586bff297b12b28ffac575c861d00c1574ee157
KatbertAllgood <srazmi@mail.ru>
2023-09-09 16:05:17 +0300

    removed unused colors

--- app/src/main/java/com/example/flippymind/ui/theme/Color.kt
+++ app/src/main/java/com/example/flippymind/ui/theme/Color.kt
@@ -2,19 +2,13 @@ package com.example.flippymind.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-//val Purple80 = Color(0xFFD0BCFF)
-//val PurpleGrey80 = Color(0xFFCCC2DC)
-//val Pink80 = Color(0xFFEFB8C8)
-//
-//val Purple40 = Color(0xFF6650a4)
-//val PurpleGrey40 = Color(0xFF625b71)
-//val Pink40 = Color(0xFF7D5260)
+
 
 internal val basePalette = FlippyMindColors(
     primaryText = Color(0xFFFFFFFF),
     primaryBackground = Color(0xFF1E2124),
     secondaryBackground = Color(0xFF36393E),
-    controlColor = Color(0xFF7289DA)
+    controlColor = Color(0xFF7289DA),
 )
 
 
