2a99a17c7582e1aeea314b5a3bc69210da34826b
KatbertAllgood <srazmi@mail.ru>
2023-09-11 18:35:13 +0300

    added database instance to App class

--- app/src/main/java/com/example/flippymind/app/App.kt
+++ app/src/main/java/com/example/flippymind/app/App.kt
@@ -1,9 +1,13 @@
 package com.example.flippymind.app
 
 import android.app.Application
+import com.example.data.database.MainDatabase
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application() {
 
+    val database by lazy {
+         MainDatabase.createDatabase(this)
+    }
 }
