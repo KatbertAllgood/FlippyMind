51d7089ded2291104327fba92e2fbbe241a36bc7
KatbertAllgood <srazmi@mail.ru>
2023-09-12 14:14:09 +0300

    created to provide usecases for deck entity

---
+++ app/src/main/java/com/example/flippymind/di/database/DeckModule.kt
@@ -0,0 +1,18 @@
+package com.example.flippymind.di.database
+
+import com.example.domain.repository.MainDatabaseRepository
+import com.example.domain.usecase.GetAllDecksUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+class DeckModule {
+
+    @Provides
+    fun providesGetAllDecksUseCase(
+        repository: MainDatabaseRepository
+    ) : GetAllDecksUseCase = GetAllDecksUseCase(repository)
+}
