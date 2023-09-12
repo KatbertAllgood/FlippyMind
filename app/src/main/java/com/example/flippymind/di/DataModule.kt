80a46f07d1238f23b1449d8d979fe07b1b0c212f
KatbertAllgood <srazmi@mail.ru>
2023-09-12 14:10:33 +0300

    data module for initiate all repositories in presentation

---
+++ app/src/main/java/com/example/flippymind/di/DataModule.kt
@@ -0,0 +1,23 @@
+package com.example.flippymind.di
+
+import android.content.Context
+import com.example.data.repository.MainDatabaseRepositoryImpl
+import com.example.domain.repository.MainDatabaseRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DataModule {
+    // all repos point there
+
+    @Provides
+    @Singleton // creates one time
+    fun provideMainDatabaseRepository(
+        @ApplicationContext context : Context
+    ) : MainDatabaseRepository = MainDatabaseRepositoryImpl(context)
+}
