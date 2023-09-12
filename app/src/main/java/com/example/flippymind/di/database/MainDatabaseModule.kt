7f14178679f98db114a34b28641b73476249aa8b
KatbertAllgood <srazmi@mail.ru>
2023-09-12 21:05:02 +0300

    added providers of insert and delete deck

--- app/src/main/java/com/example/flippymind/di/database/MainDatabaseModule.kt
+++ app/src/main/java/com/example/flippymind/di/database/MainDatabaseModule.kt
@@ -1,7 +1,9 @@
 package com.example.flippymind.di.database
 
 import com.example.domain.repository.MainDatabaseRepository
+import com.example.domain.usecase.DeleteDeckUseCase
 import com.example.domain.usecase.GetAllDecksUseCase
+import com.example.domain.usecase.InsertDeckUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,4 +17,16 @@ class MainDatabaseModule {
     fun providesGetAllDecksUseCase(
         repository: MainDatabaseRepository
     ) : GetAllDecksUseCase = GetAllDecksUseCase(repository)
+
+    @Provides
+    fun provideInsertDeck(
+        repository: MainDatabaseRepository
+    ) = InsertDeckUseCase(repository)
+
+    @Provides
+    fun provideDeleteDeck(
+        repository: MainDatabaseRepository
+    ) = DeleteDeckUseCase(repository)
+
+
 }
