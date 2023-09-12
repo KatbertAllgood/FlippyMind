bb21a4a5c90c0faea717830aac91651e62beb270
KatbertAllgood <srazmi@mail.ru>
2023-09-12 21:06:25 +0300

    added functions for delete and insert deck in database

--- app/src/main/java/com/example/flippymind/screens/mainscreen/MainScreenVM.kt
+++ app/src/main/java/com/example/flippymind/screens/mainscreen/MainScreenVM.kt
@@ -3,7 +3,10 @@ package com.example.flippymind.screens.mainscreen
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.domain.usecase.DeleteDeckUseCase
 import com.example.domain.usecase.GetAllDecksUseCase
+import com.example.domain.usecase.InsertDeckUseCase
+import com.example.flippymind.model.DeckPresentation
 import com.example.flippymind.model.mapper.ListDeckToPresentation
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -15,7 +18,9 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainScreenVM @Inject constructor(
-    private val getAllDecksUseCase: GetAllDecksUseCase
+    private val getAllDecksUseCase: GetAllDecksUseCase,
+    private val insertDeckUseCase: InsertDeckUseCase,
+    private val deleteDeckUseCase: DeleteDeckUseCase
 ) : ViewModel() {
 
     private val TAG = MainScreenVM::class.simpleName
@@ -33,4 +38,21 @@ class MainScreenVM @Inject constructor(
             }
         }
     }
+
+    fun insertDeck(
+        deck: DeckPresentation
+    ) {
+        viewModelScope.launch {
+            insertDeckUseCase(deck)
+        }
+    }
+
+    fun deleteDeck(
+        deck: DeckPresentation
+    ) {
+        viewModelScope.launch {
+            deleteDeckUseCase(deck)
+        }
+    }
+
 }
