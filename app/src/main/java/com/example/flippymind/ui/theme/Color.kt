29ac7e8c72940ab8b4ab9ab585cb1095e2bbd872
KatbertAllgood <srazmi@mail.ru>
2023-09-13 14:07:23 +0300

    added colors

--- app/src/main/java/com/example/flippymind/ui/theme/Color.kt
+++ app/src/main/java/com/example/flippymind/ui/theme/Color.kt
@@ -11,6 +11,12 @@ internal val basePalette = FlippyMindColors(
     controlColor = Color(0xFF7289DA),
 )
 
+val yellow = Color(0xFFEEAA00)
+val blue = Color(0xFF0070AF)
+val green = Color(0xFF04963E)
+val cian = Color(0xFF04B19D)
+val red = Color(0xFFB00000)
+val pink = Color(0xFFBC0071)
 
 //  maybe like this
 //val bluePalette = basePalette.copy(
