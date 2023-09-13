f46c21cd4f9ac2ff41c98ba68329fd2dbd11f90a
KatbertAllgood <srazmi@mail.ru>
2023-09-13 16:42:59 +0300

    changed colors logic and added new one color

--- app/src/main/java/com/example/flippymind/ui/theme/Color.kt
+++ app/src/main/java/com/example/flippymind/ui/theme/Color.kt
@@ -7,18 +7,21 @@ import androidx.compose.ui.graphics.Color
 internal val basePalette = FlippyMindColors(
     primaryText = Color(0xFFFFFFFF),
     primaryBackground = Color(0xFF1E2124),
-    secondaryBackground = Color(0xFF36393E),
+    secondaryBackground = Color(0xFF282B30),
+    tertiaryBackground = Color(0xFF36393E),
     controlColor = Color(0xFF7289DA),
 )
-
-val yellow = Color(0xFFEEAA00)
-val blue = Color(0xFF0070AF)
-val green = Color(0xFF04963E)
-val cian = Color(0xFF04B19D)
-val red = Color(0xFFB00000)
-val pink = Color(0xFFBC0071)
 
 //  maybe like this
 //val bluePalette = basePalette.copy(
 //    controlColor = Color(0xFF4D88FF)
 //)
+
+internal val deckPalette = FlippyMindDeckColors(
+    yellow = Color(0xFFEEAA00),
+    blue = Color(0xFF0070AF),
+    green = Color(0xFF04963E),
+    cian = Color(0xFF04B19D),
+    red = Color(0xFFB00000),
+    pink = Color(0xFFBC0071)
+)
