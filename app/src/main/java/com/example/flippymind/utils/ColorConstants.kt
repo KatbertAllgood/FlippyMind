268a3086d814e7a22f7be6568a56a9a71e183ccf
KatbertAllgood <srazmi@mail.ru>
2023-09-16 02:00:55 +0300

    added colors constants

---
+++ app/src/main/java/com/example/flippymind/utils/ColorConstants.kt
@@ -0,0 +1,12 @@
+package com.example.flippymind.utils
+
+object ColorConstants {
+
+    const val YELLOW = "yellow"
+    const val BLUE = "blue"
+    const val GREEN = "green"
+    const val CIAN = "cian"
+    const val RED = "red"
+    const val PINK = "pink"
+
+}
