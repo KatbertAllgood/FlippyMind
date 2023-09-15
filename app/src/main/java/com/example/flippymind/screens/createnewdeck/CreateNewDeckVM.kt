8c09a42bf6c796fd46bf45421ec3b26fda8d6796
KatbertAllgood <srazmi@mail.ru>
2023-09-16 02:02:44 +0300

    added insert to database function

--- app/src/main/java/com/example/flippymind/screens/createnewdeck/CreateNewDeckVM.kt
+++ app/src/main/java/com/example/flippymind/screens/createnewdeck/CreateNewDeckVM.kt
@@ -1,8 +1,11 @@
 package com.example.flippymind.screens.createnewdeck
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.domain.usecase.InsertDeckUseCase
+import com.example.flippymind.model.DeckPresentation
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,6 +15,11 @@ class CreateNewDeckVM @Inject constructor(
 
     private val TAG = CreateNewDeckVM::class.simpleName
 
-
-
+    fun insertDeck(
+        deck: DeckPresentation
+    ) {
+        viewModelScope.launch {
+            insertDeckUseCase(deck)
+        }
+    }
 }
