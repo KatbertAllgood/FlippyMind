37c7bdc39265e1720ca783e89c5afc67759b32e9
KatbertAllgood <srazmi@mail.ru>
2023-09-16 02:02:27 +0300

    unit all in one composable and began to test insert to database function

--- app/src/main/java/com/example/flippymind/screens/createnewdeck/CreateNewDeck.kt
+++ app/src/main/java/com/example/flippymind/screens/createnewdeck/CreateNewDeck.kt
@@ -1,5 +1,6 @@
 package com.example.flippymind.screens.createnewdeck
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -43,19 +44,27 @@ import androidx.compose.ui.unit.dp
 import com.example.flippymind.R
 import com.example.flippymind.ui.theme.FlippyMindSize
 import com.example.flippymind.ui.theme.FlippyMindTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.flippymind.model.DeckPresentation
+import com.example.flippymind.utils.ColorConstants
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun CreateNewDeckComposable(
-    onClickBack: () -> Unit
+    onClickBack: () -> Unit,
+    viewModel: CreateNewDeckVM = viewModel()
 ) {
     FlippyMindTheme(
         textSize = FlippyMindSize.Medium
     ) {
 
+        //region for focus
+
         val keyboardController = LocalSoftwareKeyboardController.current
         val focusManager = LocalFocusManager.current
         val interactionSource = remember { MutableInteractionSource() }
+
+        //endregion
 
         Column(
             modifier = Modifier
@@ -75,20 +84,176 @@ fun CreateNewDeckComposable(
 //            verticalArrangement = Arrangement.Center
         ) {
 
+            //region header
+
             Text(
                 text = stringResource(id = R.string.create_new_deck),
                 fontFamily = FlippyMindTheme.typography.heading.fontFamily,
                 fontSize = FlippyMindTheme.typography.heading.fontSize,
                 color = FlippyMindTheme.colors.primaryText
             )
-            
-            ColorChooser()
 
-            NameInputPlace()
+            //endregion
+
+            //region color chooser
+
+            // placed here, because it's not possible to use composable in remember
+            val deckPalette = FlippyMindTheme.deckColors
+
+            val colors = listOf(
+                Pair(ColorConstants.YELLOW, deckPalette.yellow),
+                Pair(ColorConstants.GREEN, deckPalette.green),
+                Pair(ColorConstants.RED, deckPalette.red),
+                Pair(ColorConstants.BLUE, deckPalette.blue),
+                Pair(ColorConstants.CIAN, deckPalette.cian),
+                Pair(ColorConstants.PINK, deckPalette.pink),
+            )
+
+            val (selectedColor, onColorSelected) = remember {
+                mutableStateOf(colors[0].second)
+            }
+
+            val selectedColorName = remember {
+                mutableStateOf(colors[0].first)
+            }
+
+            FlippyMindTheme {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            top = 20.dp,
+                            start = 20.dp,
+                            end = 20.dp
+                        ),
+                    colors = CardDefaults.cardColors(
+                        containerColor = FlippyMindTheme.colors.secondaryBackground
+                    ),
+                    shape = FlippyMindTheme.shape.cornersStyle,
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 5.dp
+                    )
+                ) {
+
+                    val modifier = Modifier
+//                .background(
+//                    color = FlippyMindTheme.colors.secondaryBackground,
+//                    shape = CircleShape
+//                )
+                        .scale(1.2f)
+
+                    Row(
+                        modifier = Modifier
+                            .selectableGroup()
+                            .fillMaxWidth()
+                            .height(60.dp),
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+
+                        colors.forEach { color ->
+
+                            RadioButton(
+                                selected = (color.second == selectedColor),
+                                onClick = {
+                                    onColorSelected(color.second)
+                                    selectedColorName.value = color.first
+                                          },
+                                colors = RadioButtonDefaults.colors(
+                                    selectedColor = color.second,
+                                    unselectedColor = color.second
+                                ),
+                                modifier = modifier
+                            )
+
+                        }
+                    }
+                }
+            }
+
+            //endregion
+
+            //region name input place
+
+            var name by remember {
+                mutableStateOf(
+                    TextFieldValue("")
+                )
+            }
+
+            FlippyMindTheme {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            top = 20.dp,
+                            start = 20.dp,
+                            end = 20.dp
+                        ),
+                    colors = CardDefaults.cardColors(
+                        containerColor = FlippyMindTheme.colors.secondaryBackground
+                    ),
+                    shape = FlippyMindTheme.shape.cornersStyle,
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 5.dp
+                    )
+                ) {
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(60.dp),
+                        value = name,
+                        onValueChange = { newText ->
+                            name = newText
+                        },
+                        singleLine = true,
+                        colors = TextFieldDefaults.colors(
+                            focusedTextColor = FlippyMindTheme.colors.primaryText,
+                            unfocusedTextColor = FlippyMindTheme.colors.primaryText,
+                            focusedContainerColor = FlippyMindTheme.colors.secondaryBackground,
+                            unfocusedContainerColor = FlippyMindTheme.colors.secondaryBackground,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedLabelColor = FlippyMindTheme.colors.controlColor,
+                            unfocusedLabelColor = FlippyMindTheme.colors.controlColor,
+                            cursorColor = FlippyMindTheme.colors.controlColor
+//                    focusedPlaceholderColor = FlippyMindTheme.colors.tertiaryBackground,
+//                    unfocusedPlaceholderColor = FlippyMindTheme.colors.tertiaryBackground,
+                        ),
+                        label = {
+                            Text(
+                                text = stringResource(id = R.string.name_label),
+                                fontFamily = FlippyMindTheme.typography.defaultBold.fontFamily,
+                                fontSize = FlippyMindTheme.typography.defaultBold.fontSize
+                            )
+                        },
+                        shape = FlippyMindTheme.shape.cornersStyle,
+                        placeholder = {
+                            Text(
+                                text = stringResource(id = R.string.type_name_hint)
+                            )
+                        },
+                        textStyle = TextStyle(
+                            fontFamily = FlippyMindTheme.typography.default.fontFamily
+                        )
+                    )
+                }
+            }
+
+            //endregion
+
+            //region confirm button
 
             Button(
                 onClick = {
-
+                          viewModel.insertDeck(
+                              DeckPresentation(
+                                  id = null,
+                                  name = name.text,
+                                  cardsCount = 0,
+                                  color = selectedColorName.value
+                              )
+                          )
                 },
                 shape = FlippyMindTheme.shape.cornersStyle,
                 modifier = Modifier
@@ -108,6 +273,8 @@ fun CreateNewDeckComposable(
                 )
             }
 
+            //endregion
+
         }
     }
 }
@@ -120,142 +287,142 @@ private fun CreateNewDeckPreview() {
     )
 }
 
-@Composable
-private fun ColorChooser() {
-
-    // placed here, because it's not possible to use composable in remember
-    val deckPalette = FlippyMindTheme.deckColors
-
-    val colors = listOf(
-        deckPalette.yellow,
-        deckPalette.green,
-        deckPalette.red,
-        deckPalette.blue,
-        deckPalette.cian,
-        deckPalette.pink,
-    )
-
-    val (selectedColor, onColorSelected) = remember {
-        mutableStateOf(colors[0])
-    }
-
-    FlippyMindTheme {
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    top = 20.dp,
-                    start = 20.dp,
-                    end = 20.dp
-                ),
-            colors = CardDefaults.cardColors(
-                containerColor = FlippyMindTheme.colors.secondaryBackground
-            ),
-            shape = FlippyMindTheme.shape.cornersStyle,
-            elevation = CardDefaults.cardElevation(
-                defaultElevation = 5.dp
-            )
-        ) {
-
-            val modifier = Modifier
-//                .background(
-//                    color = FlippyMindTheme.colors.secondaryBackground,
-//                    shape = CircleShape
+//@Composable
+//private fun ColorChooser() {
+//
+//    // placed here, because it's not possible to use composable in remember
+//    val deckPalette = FlippyMindTheme.deckColors
+//
+//    val colors = listOf(
+//        deckPalette.yellow,
+//        deckPalette.green,
+//        deckPalette.red,
+//        deckPalette.blue,
+//        deckPalette.cian,
+//        deckPalette.pink,
+//    )
+//
+//    val (selectedColor, onColorSelected) = remember {
+//        mutableStateOf(colors[0])
+//    }
+//
+//    FlippyMindTheme {
+//        Card(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(
+//                    top = 20.dp,
+//                    start = 20.dp,
+//                    end = 20.dp
+//                ),
+//            colors = CardDefaults.cardColors(
+//                containerColor = FlippyMindTheme.colors.secondaryBackground
+//            ),
+//            shape = FlippyMindTheme.shape.cornersStyle,
+//            elevation = CardDefaults.cardElevation(
+//                defaultElevation = 5.dp
+//            )
+//        ) {
+//
+//            val modifier = Modifier
+////                .background(
+////                    color = FlippyMindTheme.colors.secondaryBackground,
+////                    shape = CircleShape
+////                )
+//                .scale(1.2f)
+//
+//            Row(
+//                modifier = Modifier
+//                    .selectableGroup()
+//                    .fillMaxWidth()
+//                    .height(60.dp),
+//                horizontalArrangement = Arrangement.SpaceEvenly,
+//                verticalAlignment = Alignment.CenterVertically
+//            ) {
+//
+//                colors.forEach { color ->
+//
+//                    RadioButton(
+//                        selected = (color == selectedColor),
+//                        onClick = { onColorSelected(color) },
+//                        colors = RadioButtonDefaults.colors(
+//                            selectedColor = color,
+//                            unselectedColor = color
+//                        ),
+//                        modifier = modifier
+//                    )
+//
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//private fun NameInputPlace() {
+//
+//    var name by remember {
+//        mutableStateOf(
+//            TextFieldValue("")
+//        )
+//    }
+//
+//    FlippyMindTheme {
+//        Card(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(
+//                    top = 20.dp,
+//                    start = 20.dp,
+//                    end = 20.dp
+//                ),
+//            colors = CardDefaults.cardColors(
+//                containerColor = FlippyMindTheme.colors.secondaryBackground
+//            ),
+//            shape = FlippyMindTheme.shape.cornersStyle,
+//            elevation = CardDefaults.cardElevation(
+//                defaultElevation = 5.dp
+//            )
+//        ) {
+//            TextField(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .height(60.dp),
+//                value = name,
+//                onValueChange = { newText ->
+//                    name = newText
+//                },
+//                singleLine = true,
+//                colors = TextFieldDefaults.colors(
+//                    focusedTextColor = FlippyMindTheme.colors.primaryText,
+//                    unfocusedTextColor = FlippyMindTheme.colors.primaryText,
+//                    focusedContainerColor = FlippyMindTheme.colors.secondaryBackground,
+//                    unfocusedContainerColor = FlippyMindTheme.colors.secondaryBackground,
+//                    focusedIndicatorColor = Color.Transparent,
+//                    unfocusedIndicatorColor = Color.Transparent,
+//                    focusedLabelColor = FlippyMindTheme.colors.controlColor,
+//                    unfocusedLabelColor = FlippyMindTheme.colors.controlColor,
+//                    cursorColor = FlippyMindTheme.colors.controlColor
+////                    focusedPlaceholderColor = FlippyMindTheme.colors.tertiaryBackground,
+////                    unfocusedPlaceholderColor = FlippyMindTheme.colors.tertiaryBackground,
+//                ),
+//                label = {
+//                    Text(
+//                        text = stringResource(id = R.string.name_label),
+//                        fontFamily = FlippyMindTheme.typography.defaultBold.fontFamily,
+//                        fontSize = FlippyMindTheme.typography.defaultBold.fontSize
+//                    )
+//                },
+//                shape = FlippyMindTheme.shape.cornersStyle,
+//                placeholder = {
+//                    Text(
+//                        text = stringResource(id = R.string.type_name_hint)
+//                    )
+//                },
+//                textStyle = TextStyle(
+//                    fontFamily = FlippyMindTheme.typography.default.fontFamily
 //                )
-                .scale(1.2f)
-
-            Row(
-                modifier = Modifier
-                    .selectableGroup()
-                    .fillMaxWidth()
-                    .height(60.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-
-                colors.forEach { color ->
-
-                    RadioButton(
-                        selected = (color == selectedColor),
-                        onClick = { onColorSelected(color) },
-                        colors = RadioButtonDefaults.colors(
-                            selectedColor = color,
-                            unselectedColor = color
-                        ),
-                        modifier = modifier
-                    )
-
-                }
-            }
-        }
-    }
-}
-
-@Composable
-private fun NameInputPlace() {
-
-    var name by remember {
-        mutableStateOf(
-            TextFieldValue("")
-        )
-    }
-
-    FlippyMindTheme {
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    top = 20.dp,
-                    start = 20.dp,
-                    end = 20.dp
-                ),
-            colors = CardDefaults.cardColors(
-                containerColor = FlippyMindTheme.colors.secondaryBackground
-            ),
-            shape = FlippyMindTheme.shape.cornersStyle,
-            elevation = CardDefaults.cardElevation(
-                defaultElevation = 5.dp
-            )
-        ) {
-            TextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(60.dp),
-                value = name,
-                onValueChange = { newText ->
-                    name = newText
-                },
-                singleLine = true,
-                colors = TextFieldDefaults.colors(
-                    focusedTextColor = FlippyMindTheme.colors.primaryText,
-                    unfocusedTextColor = FlippyMindTheme.colors.primaryText,
-                    focusedContainerColor = FlippyMindTheme.colors.secondaryBackground,
-                    unfocusedContainerColor = FlippyMindTheme.colors.secondaryBackground,
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent,
-                    focusedLabelColor = FlippyMindTheme.colors.controlColor,
-                    unfocusedLabelColor = FlippyMindTheme.colors.controlColor,
-                    cursorColor = FlippyMindTheme.colors.controlColor
-//                    focusedPlaceholderColor = FlippyMindTheme.colors.tertiaryBackground,
-//                    unfocusedPlaceholderColor = FlippyMindTheme.colors.tertiaryBackground,
-                ),
-                label = {
-                    Text(
-                        text = stringResource(id = R.string.name_label),
-                        fontFamily = FlippyMindTheme.typography.defaultBold.fontFamily,
-                        fontSize = FlippyMindTheme.typography.defaultBold.fontSize
-                    )
-                },
-                shape = FlippyMindTheme.shape.cornersStyle,
-                placeholder = {
-                    Text(
-                        text = stringResource(id = R.string.type_name_hint)
-                    )
-                },
-                textStyle = TextStyle(
-                    fontFamily = FlippyMindTheme.typography.default.fontFamily
-                )
-            )
-        }
-    }
-}
+//            )
+//        }
+//    }
+//}
